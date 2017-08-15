c38a2818a6c58e3ff25a1f158a32e9dce96a2b2e
qins <694666422@qq.com>
2017-08-15 18:17:47 +0800

    完成 第二个 Camera ！！！
    准备做第三个的动画效果！！！

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -12,33 +13,66 @@ import android.view.View;
 
 import com.hencoder.hencoderpracticedraw4.R;
 
-public class Practice12CameraRotateFixedView extends View {
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Bitmap bitmap;
-    Point point1 = new Point(200, 200);
-    Point point2 = new Point(600, 200);
+public class Practice12CameraRotateFixedView extends View
+{
+	Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	Bitmap bitmap;
+	Point point1 = new Point(200, 200);
+	Point point2 = new Point(600, 200);
 
-    public Practice12CameraRotateFixedView(Context context) {
-        super(context);
-    }
+	public Practice12CameraRotateFixedView(Context context)
+	{
+		super(context);
+	}
 
-    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
 
-    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+	public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
 
-    {
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
-    }
+	{
+		bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		super.onDraw(canvas);
 
-        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
-        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
-    }
+		int w = bitmap.getWidth();
+		int h = bitmap.getHeight();
+
+		Camera camera = new Camera();
+		canvas.save();
+		camera.save();
+
+		//把原点移动回来
+		canvas.translate(+(w / 2 + point1.x), +(h / 2 + point1.y));
+		camera.rotateX(30);
+		camera.applyToCanvas(canvas);
+		//朱老师说 ，反的，所以原点的移动要在这里...
+		canvas.translate(-(w / 2 + point1.x), -(h / 2 + point1.y));
+
+		canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+
+
+		// 准备做第二个
+		camera.restore();
+		canvas.restore();
+
+		camera.rotateY(30);
+
+		canvas.translate(+(w / 2 + point2.x), +(h / 2 + point2.y));
+		camera.applyToCanvas(canvas);
+		canvas.translate(-(w / 2 + point2.x), -(h / 2 + point2.y));
+
+		canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+
+
+	}
 }
