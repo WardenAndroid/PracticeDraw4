6c5d8b1e8ddad346f2278c0044ffc0376040b217
qins <694666422@qq.com>
2017-08-15 16:39:26 +0800

    完成练习2
    对于练习2的 第二个效果还是通过观看源码找到解决思路的
    
    不得不说 对于  内外判断准则 还是 理解的不够深入！
    
    对于 canvas  的 save 方法也有一些理解
    
    如果是对 最地城的 Layout 进行了操作
    即便你使用 restoreToCount 也是于事无补

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -13,33 +13,55 @@ import android.view.View;
 
 import com.hencoder.hencoderpracticedraw4.R;
 
-public class Practice02ClipPathView extends View {
-    Paint paint = new Paint();
-    Bitmap bitmap;
-    Point point1 = new Point(200, 200);
-    Point point2 = new Point(600, 200);
+public class Practice02ClipPathView extends View
+{
+	Paint paint = new Paint();
+	Bitmap bitmap;
+	Point point1 = new Point(200, 200);
+	Point point2 = new Point(600, 200);
 
-    public Practice02ClipPathView(Context context) {
-        super(context);
-    }
+	public Practice02ClipPathView(Context context)
+	{
+		super(context);
+	}
 
-    public Practice02ClipPathView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public Practice02ClipPathView(Context context, @Nullable AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
 
-    public Practice02ClipPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+	public Practice02ClipPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
 
-    {
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
-    }
+	{
+		bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		super.onDraw(canvas);
 
-        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
-        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
-    }
+		int i = canvas.save();
+		canvas.save();
+		Path p1 = new Path();
+		p1.addCircle(300, 400, 100, Path.Direction.CW);
+		canvas.clipPath(p1);
+		canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+		canvas.restore();
+
+		Path p2 = new Path();
+		p2.addCircle(300 + (point2.x - point1.x), 400, 100, Path.Direction.CW);
+		p2.setFillType(Path.FillType.INVERSE_WINDING);
+		canvas.clipPath(p2);
+		canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+		canvas.restoreToCount(i);
+
+		Path p3 = new Path();
+		p3.setFillType(Path.FillType.INVERSE_EVEN_ODD);
+		p3.addCircle(300, 700, 100, Path.Direction.CW);
+		canvas.drawPath(p3, new Paint());
+	}
 }
